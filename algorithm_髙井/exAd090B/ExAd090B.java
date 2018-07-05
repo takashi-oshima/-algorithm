07fd59a3752725abb12bb0bb4f9af7b55aadfa1b
takai-yuki <takai-yuki@caica.jp>
2018-07-05 13:55:59 +0900

    作成物

--- algorithm_髙井/exAd090B/ExAd090B.java
+++ algorithm_髙井/exAd090B/ExAd090B.java
@@ -1,5 +1,11 @@
 package exAd090B;
 
+/**
+ * 入力値の間に存在する回文数を出力するクラスです。
+ *
+ * @author 高井勇輝
+ *
+ */
 public class ExAd090B {
 
 	public enum IntLength {
@@ -17,10 +23,12 @@ public class ExAd090B {
 	}
 
 	public static void main(String[] args) {
+		//入力値を受け取り適切かどうか判定し、適切で張れば変数に格納します。
 		IntegrityConfiration integrityConfiration = new IntegrityConfiration();
 		integrityConfiration.scanString();
 
+		//格納された入力値を用いて、回文数かどうかの判定を行います。
 		FindPalindrome findPalindrome = new FindPalindrome();
-		System.out.println(findPalindrome.palindromeCheck(integrityConfiration));
+		findPalindrome.palindromeCheck(integrityConfiration.getFromNumber(), integrityConfiration.getToNumber());
 	}
 }
--- algorithm_髙井/exAd098A/IntegrityConfirmation.java
+++ algorithm_髙井/exAd098A/IntegrityConfirmation.java
@@ -2,14 +2,22 @@ package exAd098A;
 
 import exAd098A.ExAd098A.IntLength;
 import exAdInput.Input;
-
+/**
+ * 入力値が指定されたものかどうか判定するクラスです。
+ *
+ * @author 高井勇輝
+ *
+ */
 public class IntegrityConfirmation {
 	/** 一つ目の入力値 */
 	int inputNumber1 = 0;
 	/** 二つ目の入力値 */
 	int inputNumber2 = 0;
+	/** 和 */
 	int sum = 0;
+	/** 差 */
 	int difference = 0;
+	/** 積 */
 	int product = 0;
 
 
@@ -55,6 +63,9 @@ public class IntegrityConfirmation {
 		this.product = product;
 	}
 
+	/**
+	 * 入力値が適切かどうかを判定します
+	 */
 	public void scanString() {
 		Input input = new Input();
 		String[] inputTxt = input.scan();
@@ -79,6 +90,13 @@ public class IntegrityConfirmation {
 		setProduct(inputNumber1 * inputNumber2);
 	}
 
+	/**
+	 * 入力値が数値に変換できるかどうかを判定します。
+	 * 変換できたときは、指定範囲内の数値であるかも判定します。
+	 *
+	 * @param inputTxt 入力文字列
+	 * @return 判定結果
+	 */
 	private boolean isIntNumber(String inputTxt) {
 		boolean isInt = false;
 		int i = 0;
