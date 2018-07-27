172fb2483658a06952bd480c7a5381afff8e8876
P18010009\CAICA <takai-yuki@caica.jp>
2018-07-27 09:42:53 +0900

    2018/07/27追加

--- src/takai/exAd071B/IntegrityConfirmation.java
+++ src/takai/exAd071B/IntegrityConfirmation.java
@@ -1,10 +1,15 @@
 package takai.exAd071B;
 
 import takai.exAdInput.Input;
-
+/**
+ *
+ *
+ * @author CAICA
+ *
+ */
 public class IntegrityConfirmation {
+	//入力されたものから英小文字以外を削除たものを入力値として格納します。
 	private String inputString = "";
-	private boolean canScan = false;
 
 	public enum TxtLength {
 		Min(1),Max(100000);
@@ -28,24 +33,10 @@ public class IntegrityConfirmation {
 	}
 
 	/**
-	 * @return canScan
-	 */
-	public boolean isCanScan() {
-		return canScan;
-	}
-
-	/**
 	 * @param inputString セットする inputString
 	 */
 	public void setInputString(String inputString) {
 		this.inputString = inputString;
-	}
-
-	/**
-	 * @param canScan セットする canScan
-	 */
-	public void setCanScan(boolean canScan) {
-		this.canScan = canScan;
 	}
 
 	/**
@@ -55,28 +46,22 @@ public class IntegrityConfirmation {
 		Input input = new Input();
 		String[] inputTxt = input.scan();
 
-		if (inputTxt.length != 1) {
-			System.out.println("不適な文字列です。");
-			System.out.println("2～100000の整数を入力してください。");
-			return;
+		for(int i = 0; i < inputTxt.length; i++) {
+			inputString += inputTxt[i].replaceAll("[^a-z]", "");
 		}
 
 		//入力された配列を数値に変換できるか試し、変換できた場合に値が適切なものか判定します。
-		if (!isTxt(inputTxt[0])) {
+		if (!isTxt(inputString)) {
 			System.out.println("不適な文字列です。");
 			System.out.println("2～100000の整数を入力してください。");
 			return;
 		}
-
-		setInputString(inputTxt[0]);
-		setCanScan(true);
 		return;
 	}
 
 	private boolean isTxt(String inputTxt) {
-		String[] splitTxt = inputTxt.replaceAll("[^a-z]", "").split("");
 
-		if(splitTxt.length < TxtLength.Min.getLength() || splitTxt.length > TxtLength.Max.getLength()) {
+		if(inputTxt.length() < TxtLength.Min.getLength() || inputTxt.length() > TxtLength.Max.getLength()) {
 			return false;
 		}
 		return true;
