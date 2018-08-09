d8ffc1d1e6d8d154cb9701e8db05fceda8a4a3f9
P18010037\CAICA <awano-shinji@caica.jp>
2018-08-09 12:53:58 +0900

    2018/08/09修正

--- src/awano/exAd071B/NotAppearAlphabet.java
+++ src/awano/exAd071B/NotAppearAlphabet.java
@@ -16,18 +16,15 @@ public class NotAppearAlphabet {
 	 */
 	public void findAlphabet(InputWord inputWord) {
 		String[] alphabetArray = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-				"r", "s",
-				"t", "u", "v", "w", "x", "y", "z" };
+				"r", "s", "t", "u", "v", "w", "x", "y", "z" };
 		String result = "";
 		for (int i = 0; i < alphabetArray.length; i++) {
 			if (!inputWord.getInputS().contains(alphabetArray[i])) {
 				result = alphabetArray[i];
-				break;
-			} else {
-				System.out.println("None");
+				System.out.println(result);
 				return;
 			}
 		}
-		System.out.println(result);
+		System.out.println("None");
 	}
 }
