65aa9d764af4b862974b7d28b7798fea14705d35
P18010010\CAICA <tsuruta-tatsuya@caica.jp>
2018-08-20 15:02:03 +0900

    no message

--- src/tsuruta/exAd099C/BankDepsits.java
+++ src/tsuruta/exAd099C/BankDepsits.java
@@ -1,7 +1,7 @@
 package exAd099C;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -10,9 +10,34 @@ import java.util.List;
  * @author 鶴田竜也
  */
 public class BankDepsits {
-
 	// キーボード入力時の限界値を格納します
 	private final int LIMITNUMBER = 100000;
+
+	/**
+	 * 引数として渡された値の累乗値を計算します。
+	 *
+	 * @param square 底の値(掛け算する数)
+	 * @return result resultリスト
+	 */
+	public List<Integer> calc(int square) {
+		List<Integer> result = new ArrayList<>();
+
+		if (square == 1) {
+			result.add(square);
+			return result;
+		}
+
+		// 指数を格納します
+		int i = 1;
+		Integer powerValue = square;
+		while (powerValue < LIMITNUMBER) {
+			result.add(powerValue);
+			powerValue = (int) Math.pow(square, i);
+			i++;
+		}
+
+		return result;
+	}
 
 	/**
 	 * N円引き出すには最低何回の操作が必要か求めます。
@@ -22,53 +47,32 @@ public class BankDepsits {
 	public void Withdrawals(InputNumber inputNumber) {
 		// 引き出し金額に入力値Nを格納します
 		int depositMoney = inputNumber.getNumberN();
-		// 9の累乗値、6の累乗値をそれぞれ格納します
-		int nineSquared = 9;
-		int sixSquared = 6;
-
-		// 操作回数を格納します
-		int manipulationTimes = 0;
 
 		// 1、6、9の累乗値を格納するためのリストを用意します
 		List<Integer> list = new ArrayList<>();
+		list.addAll(calc(1));
+		list.addAll(calc(6));
+		list.addAll(calc(9));
 
-		// 限界値(100000)までの9の累乗を入れる
-		while (LIMITNUMBER >= nineSquared) {
-			list.add(nineSquared);
-			nineSquared *= 9;
-		}
-		// 限界値(100000)までの6の累乗を入れる
-		while (LIMITNUMBER >= sixSquared) {
-			list.add(sixSquared);
-			sixSquared *= 6;
-		}
-		// 1を入れる(1の累乗はすべて1)
-		list.add(1);
+		// リストを降順に並び替えます
+		list.sort(Comparator.reverseOrder());
 
-		// リストを昇順に並び替えます
-		Collections.sort(list);
+		// 操作回数の合計を格納します
+		int totalCount = 0;
+		// リストが空じゃない且つ入力値が0円より多い時
+		while (!list.isEmpty() && depositMoney > 0) {
 
-		// 余りが0になるまで操作回数を求めます
-		while (depositMoney != 0) {
-			// listの一番大きい値から見ていく
-			for (int i = list.size() - 1; i >= 0; i--) {
-				// 引き下ろし金額が0なら終了
-				if (depositMoney == 0) {
-					break;
-				}
-				// 引き出す金額が取得した累乗値と同じかそれ以上なら
-				if (depositMoney >= list.get(i)) {
-					depositMoney -= list.get(i);
-					// 取得した要素を表示します
-					System.out.print("[" + list.get(i) + "]");
-					// 次の繰り返し時にもう一度今見たリストの要素を確認できるようにします
-					i++;
-					manipulationTimes += 1;
-				}
+			// 0番目(累乗値の最大値)から順にみていきます
+			int max = list.remove(0);
+			if (max > depositMoney) {
+				continue;
 			}
+
+			totalCount += depositMoney / max;
+			depositMoney = depositMoney % max;
 		}
 		System.out.println();
 		// 操作回数を出力します
-		System.out.println("操作回数は" + manipulationTimes + "回です");
+		System.out.println("操作回数は" + totalCount + "回です");
 	}
 }
